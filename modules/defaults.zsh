ae7e6c1d2faaaf9d7912c32d6ad06d48a3d44cde
S1cK94 <mariani.roberto94@gmail.com>
2015-04-04 20:18:43 +0200

    added $FG array to handle 256 colors

--- modules/defaults.zsh
+++ modules/defaults.zsh
@@ -2,9 +2,14 @@ autoload -U colors && colors
 
 setopt prompt_subst
 
+for color in {000..255}; do
+    FG[$color]="%{[38;5;${color}m%}"
+    BG[$color]="%{[48;5;${color}m%}"
+done
+
 PROMPT_CHAR=${PROMPT_CHAR:-"❯"}
 ACCENT_COLOR=${ACCENT_COLOR:-"$fg[green]"}
 ERROR_COLOR=${ERROR_COLOR:-"$fg[red]"}
 NORMAL_COLOR=${NORMAL_COLOR:-"$reset_color"}
-PATH_COLOR=${PATH_COLOR:-"[38;5;244m"}
-HOST_COLOR=${HOST_COLOR:-"[38;5;244m"}
+PATH_COLOR=${PATH_COLOR:-"$FG[244]"}
+HOST_COLOR=${HOST_COLOR:-"$FG[244]"}
